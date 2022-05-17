3e41d0936bd1166b10a18afcc8ce469ae3eff8ac
Enrique <enrique.casado@fppiramide.com>
2022-05-17 12:13:12 +0200

    modificado JComboBox

--- Tema 10-GUI/ejemplos/ComboBoxes.java
+++ Tema 10-GUI/ejemplos/ComboBoxes.java
@@ -1,6 +1,10 @@
 package ejemplos;
 import javax.swing.*;
+
+import ejemplosEnumeracion.Modulo;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ComponentListener;
 
 public class ComboBoxes extends JFrame {
@@ -9,16 +13,20 @@ public class ComboBoxes extends JFrame {
         setLayout(null);
         setSize(345, 120);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JComboBox modulo = new JComboBox();
+        JComboBox<String> modulo = new JComboBox<>();
         modulo.setBounds(40,10, 180, 30);
        
         modulo.addItem("Bases de Datos");
         modulo.addItem("Inglés");
         modulo.addItem("Entornos de desarrollo");
         modulo.addItem("Programación");
+        modulo.addActionListener((ae)->this.setTitle(modulo.getSelectedItem().toString()));
+        
         add(modulo);
         setVisible(true);
     }
+    
+
     
      public static void main(String[] arguments) {
         ComboBoxes cb = new ComboBoxes();
